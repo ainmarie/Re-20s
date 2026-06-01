18812b3de8c76900af68fa56d064add9a7775821
innymeeny <ainlove091911@gmail.com>
2026-06-01 16:11:03 +0900

    Initial Commit

---
+++ Re-20s/Re_20sApp.swift
@@ -0,0 +1,17 @@
+//
+//  Re_20sApp.swift
+//  Re-20s
+//
+//  Created by 김아인 on 6/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Re_20sApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Re-20sUITests/Re_20sUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Re_20sUITestsLaunchTests.swift
+//  Re-20sUITests
+//
+//  Created by 김아인 on 6/1/26.
+//
+
+import XCTest
+
+final class Re_20sUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
